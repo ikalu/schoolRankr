7eccd65c0164ba3e41b3430cbe0ecad57d3ae9ae
Sakshyam Dahal <dahalsakshyam@gmail.com>
2014-09-20 23:18:52 -0400

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index'
+  root 'welcome#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
